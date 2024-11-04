b178ee7c9b3987c470a1d4c9fc909ac2de10eda1
adarsh <adarshsomanasrt@gmail.com>
2024-11-04 13:15:08 +0530

    product update, delete , get all functionality added

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -13,7 +13,7 @@ export const addProduct = async (req, res) => {
         // Upload image
         if(req.file) {
         imageurl = await imageUploadCloudinary(req.file.path);
-        console.log('Image uploaded to Cloudinary:', imageurl)
+        // console.log('Image uploaded to Cloudinary:', imageurl)
     }
     
     // Create new product
@@ -31,5 +31,49 @@ export const addProduct = async (req, res) => {
         res.status(500).json({ message: "Creating product failed", error });
         console.error(error);
     }
-
 }
+
+//Update an exisiting product
+export const updateProduct = async (req, res) => {
+    const { id: productId } = req.params;
+    const updates = req.body;
+
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id, updates, { new: true});
+        if(!updatedProduct) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+        res.status(200).json({ message:"Product updated successfully"})
+    } catch (error) {
+        res.status(404).json({ message: "Product not found" });
+    }
+};
+
+//Delete a product
+
+export const deleteProduct = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const deletedProduct = await Product.findByIdAndDelete(id);
+        if(!deletedProduct) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+        res.status(200).json({ message: "Product deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Deleting product failed", error });
+        console.error(error);
+    }
+};
+
+// Get all products
+
+export const getAllProducts = async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({ message: "Fetching products failed", error });
+        console.error(error);
+    }
+};
