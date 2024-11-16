c9d53c639c96b5bd1b90db1e7e8954fe35dfdf0d
adarsh <adarshsomanasrt@gmail.com>
2024-11-16 21:01:15 +0530

    get all order for user and update order status for admin added

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -40,15 +40,73 @@ export const createOrder = async(req, res) => {
     }
 }
 
-// Get user orders
+// Get all orders for a user
 export const getUserOrders = async(req, res) => {
-    const userId = req.userId;
-
     try {
-        const orders = await Order.find({ userId }).populate('products.productId', 'name price' );
-        res.status(200).json(orders);
+        const userId = req.userId;
+
+        const orders = await Order.find({ user : userId })
+        .populate('orderItems.product', 'name price' )
+        .populate('shippingAddress', 'houseName locality district state pinCode')
+        .sort({ orderDate: -1 });
+
+        if(orders.length === 0) {
+            return res.status(200).json({ message: "No orders found" });
+        }
+        res.status(200).json({ message: "Orders fetched successfully", orders });
     } catch (error) {
         res.status(500).json({ message: "Fetching orders failed", error });
+        console.error(error);
+    }
+};
+
+//Get order details by order ID
+
+export const getOrderById = async(req, res) => {
+    const { id } = req.params;
+
+    try {
+        const order = await Order.findById(id)
+        .populate('user', 'username email')
+        .populate('orderItems.product', 'name price' )
+        .populate('shippingAddress', 'houseName locality district state pinCode')
+       
+
+        if(!order) {
+            return res.status(404).json({ message: "Order not found" });
+        }
+
+        res.status(200).json({ message: "Order fetched successfully", order });
+    } catch (error) {
+        res.status(500).json({ message: "Fetching order details failed", error });
+        console.error(error);
+    }
+};
+
+// Update order status (Admin feature)
+
+export const updateOrderStatus = async(req, res) => {
+    const { id } = req.params;
+    const { orderStatus } = req.body;
+
+    try {
+        const order = await Order.findById(id);
+        
+        if(!order) {
+            return res.status(404).json({ message: "Order not found" });
+        }
+
+        order.orderStatus = orderStatus;
+
+        if(orderStatus === "Delivered") {
+            order.deliveredAt = new Date();
+            order.paymentStatus = 'Paid'
+        }
+
+        await order.save();
+        res.status(200).json({ message: "Order status updated successfully", order });
+    } catch (error) {
+        res.status(500).json({ message: "Updating order status failed", error });
         console.error(error);
     }
 };
@@ -82,24 +140,24 @@ export const cancelOrder = async(req, res) => {
 
 // Update order status (admin)
 
-export const updateOrderStatus = async(req, res) => {
-    const { orderId } = req.params;
-    const { status } = req.body;
+// export const updateOrderStatus = async(req, res) => {
+//     const { orderId } = req.params;
+//     const { status } = req.body;
 
-    try {
-        const order = await Order.findByIdAndUpdate(
-            orderId, 
-            { status, updatedAt:Date.now() },
-            { new: true }); 
-        if(!order) {
-            return res.status(404).json({ message: "Order not found" });
-        }
-        res.status(200).json({ message: "Order status updated successfully", order });
-    } catch (error) {
-        res.status(500).json({ message: "Updating order status failed", error });
-        console.error(error);
-    }
-};
+//     try {
+//         const order = await Order.findByIdAndUpdate(
+//             orderId, 
+//             { status, updatedAt:Date.now() },
+//             { new: true }); 
+//         if(!order) {
+//             return res.status(404).json({ message: "Order not found" });
+//         }
+//         res.status(200).json({ message: "Order status updated successfully", order });
+//     } catch (error) {
+//         res.status(500).json({ message: "Updating order status failed", error });
+//         console.error(error);
+//     }
+// };
 
 //get all orders (Admin only)
 
