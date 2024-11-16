6ce38584bfd78dfbd2514d799ea56f528bda8d88
adarsh <adarshsomanasrt@gmail.com>
2024-11-16 21:11:04 +0530

    order routes created

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,19 +2,21 @@ import express from 'express';
 import authMiddleware from '../middleware/authMiddleware.js';
 import AdminMiddleware from '../middleware/adminMiddleware.js';
 
-import { cancelOrder, createOrder, getAllOrders, getUserOrders, updateOrderStatus } from '../controller/orderController.js';
+import { cancelOrder, createOrder, getAllOrders, getOrderById, getUserOrders, updateOrderStatus } from '../controller/orderController.js';
 
 
 const router = express.Router();
 
 // User routes
-router.post('/',authMiddleware, createOrder);
-router.get('/userOrder', authMiddleware, getUserOrders);
+router.post('/order',authMiddleware, createOrder);
+router.get('/orders', authMiddleware, getUserOrders);
+router.get('/order/:id', authMiddleware, getOrderById);
 router.patch('/cancel/:orderId', authMiddleware, cancelOrder);
 
 //Admin routes
 router.get('/admin', AdminMiddleware, getAllOrders);
-router.put('/admin/:orderId', AdminMiddleware, updateOrderStatus);
+router.put('/admin/:id/status', [authMiddleware,AdminMiddleware], updateOrderStatus);
+
 
 
 export default router;
